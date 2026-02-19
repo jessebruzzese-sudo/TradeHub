c83aceeced8fd9408c3e34d8b0a4b0e6811656eb
jessebruzzese-sudo <jessebruzzese@gmail.com>
2026-02-20 09:59:39 +1100

    fix: restore default file tracing for Vercel

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  outputFileTracing: false,
-  experimental: {
-    // Fix: ensure styled-jsx is included in Vercel serverless runtime bundle
-    outputFileTracingIncludes: {
-      '/**': ['node_modules/styled-jsx/**'],
-    },
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
