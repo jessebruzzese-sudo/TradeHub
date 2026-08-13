1a002ff49f9a94ae26f6b92f312c886808d03dec
Tim Alford <tim.alford@ockasoftware.com.au>
2026-08-13 15:34:18 +1000

    remove delete route from me, not used

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -32,47 +32,6 @@ const UpdateProfileSchema = z.object({
 	locationLng: z.string()
 });
 
-const DeleteAccountRequest = z.object({
-	password: z.string()	
-});
-
-export async function DELETE(request: NextRequest){
-	const claims = await getClaims();
-	// check payload
-	let payload = null;
-	try{
-		payload = DeleteAccountRequest.parse(await request.json());
-	}catch(err__){
-		return NextResponse.json({msg:"Invalid payload", err:err__}, {status: 400});
-	}
-	// grab user id from claims
-	const userId = claims.id;
-	const { users: userRepo } = await getDataService();
-	try{
-		// compare password
-		const match = await userRepo.validatePassword(userId, payload.password);
-		if(!match){
-			return NextResponse.json({msg: "Password doesn't match"}, {status: 401});
-		}
-	}catch(err_){
-		console.error(err_);
-		const response_ = { msg: "Failed to compare passwords", error: err_ };
-		return NextResponse.json(response_, {status: 500});
-	}
-	try{
-		// delete user
-		// and everything linked to the user
-		await userRepo.deleteUser(userId);
-		const response_ = NextResponse.json({ok: true}, {status: 200});
-		response_.cookies.delete("authorization");
-		return response_;
-	}catch(err_){
-		console.error(err_);
-		const response_ = { msg: "Failed to delete user profile", error: err_ };
-		return NextResponse.json(response_, {status: 500});
-	}
-}
-
 export async function PUT(request: NextRequest){
 	const claims = await getClaims();
 	// check payload
